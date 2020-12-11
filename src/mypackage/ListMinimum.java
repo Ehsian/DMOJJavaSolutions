6e8c806c4d75215f75b59ac74e48f89681e46749
Ben Jia <benjia279@gmail.com>
2020-12-10 16:53:31 -0800

    Create ListMinimum.java

---
+++ src/mypackage/ListMinimum.java
@@ -0,0 +1,16 @@
+package mypackage;
+import java.io.*;
+import java.util.Arrays;
+public class ListMinimum {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[]nums = new int[Integer.parseInt(br.readLine())];
+        for(int i=0;i<nums.length;i++){
+            nums[i]=Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            System.out.println(nums[i]);
+        }
+    }
+}
