9a43518dc6d18ad6eab4104e7d44fc11b678803c
Ben Jia <benjia279@gmail.com>
2020-12-11 14:45:16 -0800

    Create DMOPC14C5P2.java

---
+++ src/mypackage/DMOPC14C5P2.java
@@ -0,0 +1,20 @@
+package mypackage;
+import java.io.*;
+import java.util.StringTokenizer;
+public class DMOPC14C5P2 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer tk;
+        int num = 0;
+        int X;
+        for(int i=0;i<N;i++){
+            tk = new StringTokenizer(br.readLine());
+            X = -Integer.parseInt(tk.nextToken())+Integer.parseInt(tk.nextToken());
+            if(X>num){
+                num=X;
+            }
+        }
+        System.out.println(num);
+    }
+}
