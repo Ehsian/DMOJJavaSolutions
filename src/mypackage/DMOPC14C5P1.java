c6925fa42cdecfbf6f932b4aa8cd776438ad2b42
Ben Jia <benjia279@gmail.com>
2020-12-21 16:49:32 -0800

    Create DMOPC14C5P1.java

---
+++ src/mypackage/DMOPC14C5P1.java
@@ -0,0 +1,10 @@
+package mypackage;
+import java.io.*;
+import java.io.InputStreamReader;
+
+public class DMOPC14C5P1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println((3.14159*Math.pow(Integer.parseInt(br.readLine()),2)*Integer.parseInt(br.readLine()))/3);
+    }
+}
