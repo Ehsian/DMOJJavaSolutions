a2360b004327a7af327e264984e30f425ce4c003
Ben Jia <benjia279@gmail.com>
2021-01-06 16:04:46 -0800

    Create CCC09J1.java

---
+++ src/mypackage/CCC09J1.java
@@ -0,0 +1,8 @@
+package mypackage;
+import java.io.*;
+public class CCC09J1 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("The 1-3-sum is "+(91+Integer.parseInt(br.readLine())+Integer.parseInt(br.readLine())*3+Integer.parseInt(br.readLine())));
+    }
+}
