7f6383786d0967be02c8e7e91cc809bd01d61de3
Ben Jia <benjia279@gmail.com>
2021-01-06 16:04:14 -0800

    Create CCC14J3.java

---
+++ src/mypackage/CCC14J3.java
@@ -0,0 +1,22 @@
+package mypackage;
+import java.io.*;
+import java.util.StringTokenizer;
+public class CCC14J3 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int Antonia = 100,David = 100;
+        int N = Integer.parseInt(br.readLine());
+        for(int i=0;i<N;i++){
+            StringTokenizer tk = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(tk.nextToken());
+            int d = Integer.parseInt(tk.nextToken());
+            if(a>d){
+                David-=a;
+            }else if(d>a){
+                Antonia-=d;
+            }
+        }
+        System.out.println(Antonia);
+        System.out.println(David);
+    }
+}
