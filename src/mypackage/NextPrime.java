cb215592f0804a3b0535873534f2d5140099773c
Ben Jia <benjia279@gmail.com>
2021-01-06 16:01:24 -0800

    Create NextPrime.java

---
+++ src/mypackage/NextPrime.java
@@ -0,0 +1,14 @@
+package mypackage;
+import java.math.BigInteger;
+import java.io.*;
+
+public class NextPrime {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BigInteger N = new BigInteger(br.readLine());
+        while(N.isProbablePrime(1)!=true){
+            N = new BigInteger(""+(Integer.parseInt(""+N)+1));
+        }
+        System.out.println(N);
+    }
+}
