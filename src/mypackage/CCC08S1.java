88cadd1257bd1a3e4692596eee03f2321e329c2e
Ben Jia <benjia279@gmail.com>
2021-01-06 16:05:02 -0800

    Create CCC08S1.java

---
+++ src/mypackage/CCC08S1.java
@@ -0,0 +1,27 @@
+package mypackage;
+import java.io.*;
+import java.util.StringTokenizer;
+public class CCC08S1 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String lowestStr = null;
+        int lowestNum = 0;
+        while(true){
+            StringTokenizer tk = new StringTokenizer(br.readLine());
+            String str = tk.nextToken();
+            int num = Integer.parseInt(tk.nextToken());
+            if(lowestStr==null){
+                lowestStr = str;
+                lowestNum = num;
+            }
+            else if(num<lowestNum){
+                lowestStr = str;
+                lowestNum = num;
+            }
+            if(str.equals("Waterloo")){
+                break;
+            }
+        }
+        System.out.println(lowestStr);
+    }
+}
