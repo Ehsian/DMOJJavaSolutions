d891272ddf356313769bf46e0dfa9df46877bfe6
Ben Jia <benjia279@gmail.com>
2021-01-06 16:04:42 -0800

    Create CCC11J3.java

---
+++ src/mypackage/CCC11J3.java
@@ -0,0 +1,17 @@
+package mypackage;
+import java.io.*;
+public class CCC11J3 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t1 = Integer.parseInt(br.readLine());
+        int t2 = Integer.parseInt(br.readLine());
+        int ans = 2;
+        while(t2<=t1){
+            int T=t1;
+            t1=t2;
+            t2=T-t2;
+            ans++;
+        }
+        System.out.println(ans);
+    }
+}
