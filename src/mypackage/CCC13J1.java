862ad540b0cae69c9f7da2f2dd57a9c9e7282f03
Ben Jia <benjia279@gmail.com>
2021-01-06 16:04:31 -0800

    Create CCC13J1.java

---
+++ src/mypackage/CCC13J1.java
@@ -0,0 +1,8 @@
+package mypackage;
+import java.io.*;
+public class CCC13J1 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(-Integer.parseInt(br.readLine())+Integer.parseInt(br.readLine())*2);
+    }
+}
