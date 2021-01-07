9667e216c30b4342bbf50324ba477cf8ab1b1b37
Ben Jia <benjia279@gmail.com>
2021-01-06 16:05:11 -0800

    Create CCC04J1.java

---
+++ src/mypackage/CCC04J1.java
@@ -0,0 +1,8 @@
+package mypackage;
+import java.io.*;
+public class CCC04J1 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("The largest square has side length "+(int)Math.floor(Math.sqrt(Integer.parseInt(br.readLine())))+".");
+    }
+}
