bc8b7a8934b5fa799a5b09c86122a4ba5769bd26
Ben Jia <benjia279@gmail.com>
2021-07-17 12:27:23 -0700

    Update

--- src/mypackage/AvocadoThief.java
+++ src/mypackage/AvocadoThief.java
@@ -10,13 +10,13 @@ public class AvocadoThief {
         int M = Integer.parseInt(tk.nextToken());
         int K = Integer.parseInt(tk.nextToken());
         tk = new StringTokenizer(br.readLine());
-        int A = Integer.parseInt(tk.nextToken());
-        int B = Integer.parseInt(tk.nextToken());
+        int X = Integer.parseInt(tk.nextToken());
+        int Y = Integer.parseInt(tk.nextToken());
         int[][]map = new int[N][N];
         for(int i=0;i<M;i++){
             tk = new StringTokenizer(br.readLine());
-            int b1 = Integer.parseInt(tk.nextToken());
-            int b2 = Integer.parseInt(tk.nextToken());
+            int b1 = Integer.parseInt(tk.nextToken())-1;
+            int b2 = Integer.parseInt(tk.nextToken())-1;
             if(tk.nextToken().equals("R")){
                 map[b1][b2]=0;
                 map[b2][b1]=0;
@@ -26,8 +26,7 @@ public class AvocadoThief {
                 map[b2][b1]=1;
             }
         }
-        BFS(A,N,map);
-        System.out.println();
+        System.out.println(BFS(X,N,map));
         if(false){
             System.out.println("Caught");
         } else{
@@ -35,28 +34,28 @@ public class AvocadoThief {
         }
     }
     public static int BFS(int start,int N,int[][]map){
-        System.out.println("BFS STarted");
+        System.out.println("BFS Started");
         HashSet<Integer>visited = new HashSet<>();
         Queue<Integer>queue = new ArrayDeque<>();
         int mins = 0;
         queue.add(start);
         while(!queue.isEmpty()){
             int currentbuilding = queue.poll();
-            if(currentbuilding==N){
+            if(currentbuilding==N-1){
                 return mins;
             }
             if(!visited.contains(currentbuilding)){
                 visited.add(currentbuilding);
                 for(int i=0;i<N;i++){
-                    if(map[currentbuilding-1][i]==0){
+                    if(map[currentbuilding][i]==0){
                         queue.add(i);
                     }
-                    else if(map[currentbuilding-1][i]==1){
+                    else if(map[currentbuilding][i]==1){
                         queue.add(i);
                     }
                 }
+                mins++;
             }
-            mins++;
         }
         return -1;
     }
