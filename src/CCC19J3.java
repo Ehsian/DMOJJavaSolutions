7b1e3bd96c27715f4f779237414e3855cd1c40f0
Ben <benjia279@gmail.com>
2023-01-23 10:49:23 -0800

    Back to doing DMOJ

--- src/CCC19J3.java
+++ src/CCC19J3.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class CCC19J3 {
+import java.util.*;
+import java.io.*;
+public class CCC19J3 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for(int i=0;i<N;i++){
+            String str = br.readLine();
+            char prev = str.charAt(0);
+            int occ = 1;
+            for(int j=1;j<str.length();j++){
+                if(str.charAt(j)==prev){
+                    occ++;
+                }
+                else{
+                    System.out.print(occ + " " + prev + " ");
+                    occ = 1;
+                    prev = str.charAt(j);
+                }
+            }
+            System.out.println(occ + " " + prev);
+        }
+    }
 }
